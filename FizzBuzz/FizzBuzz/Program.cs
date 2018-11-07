a0bb40f19bb67e13376dbae0b9686634f6f045f8
Joseph Gage <Joseph.Gage@softwire.com>
2018-11-07 09:41:38 +0000

    Add Bong and Fezz rules

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -8,12 +8,17 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int counter = 1; counter <= 100; counter++)
+            for (int counter = 1; counter <= 150; counter++)
             {
                 List<string> rules = new List<string>();
                 if (counter % 3 == 0)
                 {
                     rules.Add("Fizz");
+                }
+
+                if (counter % 13 == 0)
+                {
+                    rules.Add("Fezz");
                 }
 
                 if (counter % 5 == 0)
@@ -24,6 +29,14 @@ namespace FizzBuzz
                 if (counter % 7 == 0)
                 {
                     rules.Add("Bang");
+                }
+
+                if (counter % 11 == 0)
+                {
+                    rules.Remove("Fizz");
+                    rules.Remove("Buzz");
+                    rules.Remove("Bang");
+                    rules.Add("Bong");
                 }
 
                 string output = String.Join("", rules);
